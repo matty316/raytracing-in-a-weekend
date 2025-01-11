0fa2869c04a083d28b40e2451623538c23cf666a
matty 3:16 <mattreed1890@gmail.com>
2025-01-11 08:02:16 -0600

    Initial commit

---
+++ premake5.lua
@@ -0,0 +1,21 @@
+workspace "WorkspaceName"
+    configurations { "Debug", "Release" }
+    platforms { "Win64" }
+
+project "ProjectName"
+    kind "ConsoleApp"
+    language "C++"
+    targetdir "bin/%{cfg.buildcfg}"
+
+    files { "include/**.h", "src/**.c", "src/**.cpp" }
+
+    includedirs { "include" }
+    includedirs { "src" }
+
+    filter "configurations:Debug"
+    defines { "DEBUG" }
+    symbols "On"
+
+    filter "configurations:Release"
+    defines { "NDEBUG" }
+    optimize "On"
